1ecffffbab8e3af102e8a7687e8efdcca5db0dcc
Sneakup <dylan15766@hotmail.co.uk>
2017-07-26 23:54:13 +0100

    Add files via upload

--- mods.py
+++ mods.py
@@ -10,8 +10,8 @@ table = dynamodb.Table('interview_test')
 def display(id):
     
     try: # find instance in database
-        response = table.get_item(Key={'instance_id': id,})
-        item = response['Item']
+        db_attribute_ = table.get_item(Key={'instance_id': id,})
+        item = db_instance_info['Item']
         #print json (I think)
         for key, value in item.items() :
             print (key+': ', value)
@@ -20,7 +20,7 @@ def display(id):
 
 def scan():
     # Get information for all instances
-    running_instances = ec2.instances.filter(Filters=[{ 'Name': 'instance-state-name', 'Values': ['*']}])
+    running_instances = ec2.instances.filter()
 
     ec2info = defaultdict()
 
@@ -32,26 +32,26 @@ def scan():
         # Add instance info to a dictionary         
         ec2info[instance.id] = {'Instance Id': instance.instance_id,'Type': instance.instance_type,'State': instance.state['Name'],'Image Id': instance.image_id}
 
-    attributes = ['Instance Id', 'State', 'Type', 'Image Id']
-    titles = ['instance_id', 'State_Name', 'InstanceType', 'ImageId']
+    ec2_instance_keys = ['Instance Id', 'State', 'Type', 'Image Id']
+    db_attribute_id = ['instance_id', 'State_Name', 'InstanceType', 'ImageId']
 
     #print and store in database
     for instance_id, instance in ec2info.items():
-        stuff = []
-        for key in attributes:
+        ec2_instance_info = []
+        for key in ec2_instance_keys:
             print("{0}: {1}".format(key, instance[key]))
-            stuff.append(instance[key])
+            ec2_instance_info.append(instance[key])
         print("------")
 
         try: #check if instance id in database. If true, update each value.
-            table.get_item(Key={'instance_id': stuff[0]})
+            table.get_item(Key={'instance_id': ec2_instance_info[0]})
             counter = 1
             #iterate through each 'column' in 'row' and replace value.
-            for item in stuff:
-                table.update_item(Key={'instance_id': stuff[0]},UpdateExpression='SET '+titles[counter]+' = :val1', ExpressionAttributeValues={':val1': item})
+            for item in ec2_instance_info:
+                table.update_item(Key={'instance_id': ec2_instance_info[0]},UpdateExpression='SET '+db_attribute_id[counter]+' = :val1', ExpressionAttributeValues={':val1': item})
                 counter += 1
         except:#Else, insert new data.
-                table.put_item(Item={'instance_id': stuff[0],'State_Name': stuff[1],'InstanceType': stuff[2],'ImageId': stuff[3]})
+                table.put_item(Item={'instance_id': ec2_instance_info[0],'State_Name': ec2_instance_info[1],'InstanceType': ec2_instance_info[2],'ImageId': ec2_instance_info[3]})
 
 def remove(id):
     try: 
@@ -60,8 +60,8 @@ def remove(id):
     except: print("Instance not found/deleted")
 
 def all():
-    response = table.scan(('instance_id'))
-    results = response['Items']
+    db_instance_info = table.scan(('instance_id'))
+    results = db_instance_info['Items']
     for item in results:
         for key, value in item.items():
             print (key+': ', value)
